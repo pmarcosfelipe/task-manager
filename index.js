87e518ca22f61c4ebd2b6d03364e7c51b8ce8764
Marcos Felipe <p.marcosfelipe@gmail.com>
2022-01-16 17:49:46 +0000

    add NotFound handler middleware

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
 
 require('dotenv').config();
 
+app.use(express.static('./public'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Task Manager App');
-});
-
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
 
 const start = async () => {
   try {
