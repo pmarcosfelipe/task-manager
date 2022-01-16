2aece160bfaf86b3b0bfff80f25c983f0c4aa490
Marcos Felipe <p.marcosfelipe@gmail.com>
2022-01-16 16:08:09 +0000

    create Task model and update createTask function on controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,12 @@
+const Task = require('../models/Task');
+
 const getTasks = (req, res) => {
   res.send('get all tasks');
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 
 const updateTask = (req, res) => {
